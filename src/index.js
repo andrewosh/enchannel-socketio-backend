195545776b7923eced77453565f17b614fc3265e
andrewosh <andrewosh@gmail.com>
2016-03-01 21:47:06 -0500

    Got the socket.io backend working with remote kernels

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-const request = require('browser-request');
+const request = require('request');
 const urljoin = require('url-join');
-const io = require('socket.io-client');
 const connectionsMap = {};
 const rxjs = require('@reactivex/rxjs');
 const { Subscriber, Observable, Subject } = rxjs;
@@ -49,6 +48,7 @@ function rp(url) {
   return new Promise((resolve, reject) => {
     request(url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
+        console.log('BACKEND GOT: ' + JSON.stringify(body))
         resolve(body);
       } else {
         reject(body);
@@ -61,20 +61,33 @@ export function spawn(endpoint, kernelName) {
   return rp(urljoin(endpoint, 'spawn', kernelName)).then(x => JSON.parse(x).id );
 }
 
+export function list(endpoint) {
+  return rp(urljoin(endpoint, 'list')).then(x => JSON.parse(x))
+}
+
+export function specs(endpoint) {
+  console.log('BACKEND: getting all specs at ' + urljoin(endpoint, 'specs'))
+  return rp(urljoin(endpoint, 'specs')).then(x => JSON.parse(x))
+}
+
 export function connect(endpoint, kernelId) {
+  var url = require('url').parse(endpoint)
+  var path = url.path + 'socket.io/'
+  const Manager = require('socket.io-client').Manager
+  const Socket = require('socket.io-client').Socket
+  var io = Manager(url.href, { path })
   return new Promise(resolve => {
-    var socket = io.connect(urljoin(endpoint, 'shell', kernelId));
     var connections = connectionsMap[kernelId] = {
-      shell: io.connect(urljoin(endpoint, 'shell', kernelId)),
-      stdio: io.connect(urljoin(endpoint, 'stdio', kernelId)),
-      iopub: io.connect(urljoin(endpoint, 'iopub', kernelId)),
-      control: io.connect(urljoin(endpoint, 'control', kernelId)),
+      shell: new Socket(io, '/shell/' + kernelId).connect(),
+      stdio: new Socket(io, '/stdio/' + kernelId).connect(),
+      iopub: new Socket(io, '/iopub/' + kernelId).connect(),
+      control: new Socket(io, '/control/' + kernelId).connect()
     };
     resolve({
       shell: createSubject(connections.shell),
       control: createSubject(connections.control),
       iopub: createSubject(connections.iopub),
-      stdio: createSubject(connections.stdio),
+      stdio: createSubject(connections.stdio)
     });
   });
 }
